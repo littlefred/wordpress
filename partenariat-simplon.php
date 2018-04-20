5219e462ec7cb8b5da51f43be80939c83b3da135
Frederick <Frederick@192.168.1.160>
2018-04-20 10:17:49 +0200

    correction lien plugin

--- partenariat-simplon.php
+++ partenariat-simplon.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Simplon plugin
-Plugin URI: 
+Plugin URI: http://192.168.1.160//Users/Frederick/eclipse-workspace/wordpress/README.md
 Description: Un plugin d'ajout de la mention simplon pour le développement sous WordPress
 Version: 0.1
 Author: Frederick PAREJA
@@ -10,7 +10,7 @@ License: formation wordpress
 */
 
 function simplonMention() {
-	return '<p>La publication de cet article est possible grâce à mon super partenaire <a href="https://simplon.co">simplon.co</a> - une entreprise de
+	return '<p>La publication de cet article est possible grâce à mon super partenaire <a href="https://simplon.co" target="_blank">simplon.co</a> - une entreprise de
 l’économie sociale et solidaire et un réseau de « fabriques » (écoles) qui propose des formations GRATUITES pour devenir développeur web.</p>';
 }
 add_shortcode('simplon', 'simplonMention');
